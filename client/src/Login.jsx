82e83127a0e528f56b8a14c5cbc2bcb949aba538
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2024-11-10 21:01:08 +0700

    tes

---
+++ client/src/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Tambahkan logika submit di sini, seperti mengirim data ke backend
+    console.log({ name, email, password });
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              autoComplete="off"
+              className="form-control rounded-0"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              className="form-control rounded-0"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              autoComplete="off"
+              className="form-control rounded-0"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Login
+          </button>
+          <p>Not ready Have an Account</p>
+
+          <Link
+            to="/register"
+            type="submit"
+            className="btn btn-default border w-100 bg-light rounded-0 "
+          >
+            Register
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
